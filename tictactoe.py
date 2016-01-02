56c1cc3cc7bb496fbf7050f5146165819ce94d4c
NoBro93 <ngb1311@yahoo.com>
2016-01-01 19:03:19 -0500

    players, turns

--- tictactoe.py
+++ tictactoe.py
@@ -2,9 +2,6 @@
 #maybe have options for 1 player or 2 player
 
 #TODO:
-#board - array from 0 to 8 left to right, then down
-#players - either 2 players or 1 + computer
-#how to take a turn - ask for a move, check if valid, store move
 #AI - decide which move is best - see xkcd?, maybe have easy/hard modes
 #how to check for wins - long way?
 
@@ -18,8 +15,37 @@ def printBoard(board):
         elif i<8:
             print "___|___|___"
     print
-printBoard([1,2,3,4,5,6,7,8,9])
 
-#MAIN
+def turn(player, board):
+    while True:
+        move = raw_input("Where would you like to move?")
+        try:                            #in case a noninteger is input
+            move = int(move)
+        except:
+            print "Please enter an integer"
+            continue
+        if move >= 1 and move <=9:      #make sure number is in range of board
+            if board[move - 1] == ' ':      #if empty slot
+                board[move - 1] = player    #set slot to that player
+                break
+            else:
+                print "That slot is not available"
+        else:
+            print "Number not in range (1-9)"
+    printBoard(board)
+
+#INITIALIZATIONS
 board = [' ',' ',' ',' ',' ',' ',' ',' ',' '] #initialize board as empty
-printBoard(board)
+player = raw_input("Would you like to be X or O?")[0] #the [0] is in case they enter more than 1 character
+if player == 'X':   #may ask second player if one exists
+    computer = 'O'
+else:
+    computer = 'X'
+
+#GAMEPLAY
+printBoard([1,2,3,4,5,6,7,8,9]) #use to show the options for spaces
+for i in range(9): #for now, play as if 2 players, later add a step to calculate the computer's move
+    if i%2 == 0:
+        turn(player, board)
+    else:
+        turn(computer, board)
